f51d97d3d5da81cfd47cd2b14cdc5e2d3adbfd58
alokdangre <alokdangre@gmail.com>
2025-09-15 18:23:30 +0530

    fixed lint

--- frontend/e2e/Install.spec.ts
+++ frontend/e2e/Install.spec.ts
@@ -1,4 +1,4 @@
-import {test, expect} from '@playwright/test'
+import {test} from '@playwright/test'
 
 test('install',async({page}) => {await page.goto('http://localhost:5173/install');
     await page.getByRole('heading', { name: 'Welcome to KubeStellar' }).click();
